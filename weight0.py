cf04522a57aacc55b314bcddf04790b5baec26ea
SALMA55ASHRAF <116911551+SALMA55ASHRAF@users.noreply.github.com>
2023-09-11 22:46:02 +0300

    Update weight0.py

--- weight0.py
+++ weight0.py
@@ -1,10 +1,1 @@
-weight=int(input('weight : '))
-c=0
-unit=input('L(bs) or K(g) : ')
-if unit.upper()=='L':
-    c=weight *0.45
-    print(f'you are {c} kilos ')
-else:
-    c=weight/0.45
-    print(f'you are {c} pounds ')
 
